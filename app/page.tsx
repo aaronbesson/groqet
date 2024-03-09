b873b31a060c44157ad468f839886cae7ece9437
AaronB <aaronbesson@gmail.com>
2024-03-08 21:54:06 -0400

    added Groqet logo

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,9 @@ import { CloudArrowDownIcon, RocketLaunchIcon } from "@heroicons/react/24/solid"
 import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 import Typewriter from "./components/Typewriter";
+import Lottie from "lottie-react";
+import groqet from "../public/groqet.json"
+
 
 const llmModels = [
   {
@@ -17,7 +20,7 @@ const llmModels = [
   }
 ]
 
-interface Window {
+interface Window { // add webkitSpeechRecognition to window
   webkitSpeechRecognition: any;
 }
 
@@ -42,18 +45,16 @@ export default function Home() {
   const [reply, setReply] = useState("");
   const [recognition, setRecognition] = useState(null as any);
   const [isListening, setIsListening] = useState(false);
-  const [audio, setAudio] = useState(null as any);
 
   const getAudioData = async (message: any) => {
     try {
       const reqBody = { message };
-      const response = await fetch('/api/googletts', {
+      const response = await fetch('/api/googletts', { // send request to google tts api
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(reqBody)
       });
-      const data = await response.json();
-      // Ensure proper data URI format for audio
+      const data = await response.json(); // Ensure proper data URI format for audio
       const audioSrc = `data:audio/mp3;base64,${data.audioContent}`;
       const audio = new Audio(audioSrc);
       audioRef.current = audio;
@@ -63,9 +64,9 @@ export default function Home() {
       console.error('Error:', error);
       alert('Error!' + error);
     }
-};
+  };
 
-  const sendMessage = async (recordedText: any) => {
+  const sendMessage = async (recordedText: any) => { // send message to groq api
     setLoading(true);
     const newMessage = { // add prompt to messages
       type: "user",
@@ -97,7 +98,6 @@ export default function Home() {
       if (isListening) { // if the app is listening, play the audio
         getAudioData(data.output);
       }
-
       setMessages(prevMessages => [...prevMessages, newMessage]);
       setLoading(false);
     } catch (error) {
@@ -105,10 +105,9 @@ export default function Home() {
       alert('Error!' + error)
     }
   };
-  
 
   useEffect(() => {
-    const handleClick = (e: MouseEvent) => {
+    const handleClick = (e: MouseEvent) => { // close model on click outside
       if (modelRef.current && !modelRef.current.contains(e.target as Node)) {
         setShowModel(false);
       }
@@ -119,15 +118,13 @@ export default function Home() {
     }
   }, [modelRef]);
 
-
-// stop all audio on click
-  const handleClick = () => {
+  const handleClick = () => { // stop audio on click
     if (audioRef.current) {
       audioRef.current.pause();
     }
   }
 
-  
+
   useEffect(() => { // check if speech recognition is available
     if ('webkitSpeechRecognition' in window) {
       const recognition = new (window as Window).webkitSpeechRecognition();
@@ -150,7 +147,7 @@ export default function Home() {
     }
   }, []);
 
-  const startListening = () => {
+  const startListening = () => { // start listening to microphone
     setReply("Hello! I'm Groqet, start speaking to ask me a question.")
     setIsListening(true);
     if (recognition) {
@@ -158,7 +155,7 @@ export default function Home() {
     }
   };
 
-  const stopListening = () => {
+  const stopListening = () => { // stop listening
     setIsListening(false);
     if (recognition) {
       recognition.stop();
@@ -176,13 +173,13 @@ export default function Home() {
     a.click();
   };
 
-  useEffect(() => {
+  useEffect(() => { // get audio data when reply changes
     if (reply !== "" && isListening) {
       getAudioData(reply);
     }
   }, [reply]);
-  
-  useEffect(() => {
+
+  useEffect(() => { // Try scroll to bottom of chat window
     if (windowRef.current) {
       const scrollHeight = windowRef.current.scrollHeight;
       windowRef.current.scrollTop = scrollHeight
@@ -191,15 +188,15 @@ export default function Home() {
 
   return (<div>
     {Header()}
-    <main className="flex flex-col items-center justify-between w-full h-svh">
+    <main className="flex flex-col items-center justify-between w-full h-svh font-mono">
       <div
         ref={windowRef}
         className="flex-1 overflow-auto flex flex-col gap-4 w-full h-svh py-12"
       >
         {messages.map((item, index) => item.value !== "" &&
-          <div 
-          onClick={() => handleClick()}
-          key={index} className={`mx-6 shadow border items-start rounded-2xl md:items-center max-w-5xl ${item.type === "user" ? "text-right self-end bg-green-100 ml-24" : "bg-white mr-24"}`}>
+          <div
+            onClick={() => handleClick()}
+            key={index} className={`mx-6 shadow border items-start rounded-2xl md:items-center max-w-5xl ${item.type === "user" ? "text-right self-end bg-green-100 ml-24" : "bg-white mr-24"}`}>
             <Typewriter
               fontSize={16}
               delay={0}
@@ -207,11 +204,21 @@ export default function Home() {
               text={item.value}
             />
           </div>)}
+
+        {messages.length > 0 &&
+          <div className="text-center flex flex-col gap-2">
+            <Lottie animationData={groqet} loop={true} className="w-32 h-32 mx-auto" />
+            Welcome to Groqet
+          </div>}
+
       </div>
-      <div className="p-2 flex items-center gap-4 w-full bg-gray-100 px-4">
+
+      <div className="p-2 flex items-center gap-4 w-full bg-gray-100 px-4 border-t border-gray-200">
+
         <CloudArrowDownIcon
-        onClick={downloadMessages}
-        className="h-6 w-6 text-gray-500 hover:text-black cursor-pointer" />
+          onClick={downloadMessages}
+          className="h-6 w-6 text-gray-500 hover:text-black cursor-pointer" />
+
         <input
           value={prompt}
           onChange={(e) => setPrompt(e.target.value)}
@@ -221,8 +228,9 @@ export default function Home() {
             }
           }}
           className="flex bg-white w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 flex-1"
-          placeholder="Ask me a question."
+          placeholder="Ask me a question"
         />
+
         <button
           disabled={loading}
           onClick={() => sendMessage(prompt)}
@@ -230,24 +238,27 @@ export default function Home() {
           {loading ? <ArrowPathIcon className="h-6 w-6 animate-spin" aria-hidden="true" />
             : "Send"}
         </button>
-      {!isListening ?  <MicrophoneIcon
-      onClick={startListening}
-      className="h-6 w-6 text-gray-500 hover:text-black cursor-pointer" /> : <MicrophoneIcon
-      onClick={stopListening}
-      className="h-6 w-6 text-red-500 animate-pulse cursor-pointer" />}
+
+        {!isListening ?
+          <MicrophoneIcon
+            onClick={startListening}
+            className="h-6 w-6 text-gray-500 hover:text-black cursor-pointer" />
+          :
+          <MicrophoneIcon
+            onClick={stopListening}
+            className="h-6 w-6 text-red-500 animate-pulse cursor-pointer" />
+        }
       </div>
     </main>
   </div>
   );
 
   function Header() {
-    return <div className="flex fixed top-0 py-1 gap-2 justify-between w-full px-2 items-center" style={{
-      backgroundColor: 'rgba(214, 219, 220,1)'
-    }}>
+    return <div className="flex fixed top-0 py-1 gap-2 justify-between w-full px-2 items-center font-mono">
 
       <div
         onClick={() => setShowModel(!showModel)}
-        className="flex text-xs gap-2 cursor-pointer opacity-50 hover:opacity-90 bg-white rounded-lg px-2 py-1 items-center hover:shadow">
+        className="flex text-xs gap-2 cursor-pointer opacity-50 hover:opacity-90 bg-white rounded-lg px-2 py-1 items-center hover:shadow transition-all duration-700">
         <RocketLaunchIcon className="h-5 w-5" />
         <p className="text-center">
           {model}
@@ -256,12 +267,12 @@ export default function Home() {
       <div
         ref={modelRef}
         className={`
-      ${showModel ? "absolute h-auto" : "hidden h-0"}
-       left-3 top-8 bg-white border h-auto p-2 rounded-lg rounded-t-none hover:shadow-xl transition-all cursor-pointer duration-700`}>
+      ${showModel ? "left-8" : "-left-48"}
+      absolute  top-10 bg-white border p-2 rounded-lg opacity-50 hover:opacity-90 hover:shadow-xl transition-all cursor-pointer duration-700 flex-col gap-2`}>
         {llmModels.map((llm) => (
           <div
             onClick={() => { setModel(llm.model); setShowModel(false); }}
-            key={llm.id} className="flex gap-2 items-center opacity-50 hover:opacity-90 text-sm">
+            key={llm.id} className="flex gap-2 items-center text-xs text-gray-400 hover:text-gray-700">
             {llm.model}
           </div>
         ))}
